ef8a44b7e7decff1eab22cc2292343b9596630d5
Scott Holland <scotthollandjr@gmail.com>
2016-01-03 20:33:17 -0800

    Edit receipt project

--- js/receipt.js
+++ js/receipt.js
@@ -1,1 +1,17 @@
-receipt.js
+$(document).ready(function() {
+  $("#verify form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var numberInput = $("input#number").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+
+    $("span#nameRec").append(nameInput);
+    $("span#numberRec").append(numberInput);
+    $("span#streetRec").append(streetInput);
+    $("span#cityRec").append(cityInput);
+
+    $("#verify").hide();
+    $("#receipt").show();
+    event.preventDefault();
+  });
+});
